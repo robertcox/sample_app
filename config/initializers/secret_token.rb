dd26e8c60d318ce785eaeb569f995bb45813dbdd
robertcox <robert.m.cox@gmail.com>
2013-10-16 19:10:22 -0400

    fixed the broken stuff

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,3 +10,4 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 SampleApp::Application.config.secret_key_base = 'f862fe79002ba274ad564e61cf4b2dcafeca2dd72cfa15044c1cbacad3927086ec27a6a7d8536b751cebfc7d65b8610c1210d53fe3bcb7de9f17d03a07988d41'
+SampleApp::Application.config.secret_token = "some secret phrase of at least 30 characters"
